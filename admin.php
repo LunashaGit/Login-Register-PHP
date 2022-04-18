47130118a250940ceb87e67b890adbb05731e4a7
Luna Muylkens <93606228+LunashaGit@users.noreply.github.com>
2022-04-18 11:16:28 +0200

    Admin Name + Hey Fix

--- admin.php
+++ admin.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['username']) && !isset($_SESSION['permission'])) {
 <?php include './parts/head.php'; ?>
 <body>
 	<div class="container">
-		<h1>Welcome Back Admin Luna!</h1>
+		<h1><?= "Welcome Back Admin " . $_SESSION['username'] . " !"?></h1>
         <a href="logout.php">Logout</a>
 	</div>
 </body>
